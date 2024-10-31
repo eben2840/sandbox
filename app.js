7091138ab328ecb540a380245d43b43aa6bd56b6
Ebenezer Aggrey-Mills <106875822+eben2840@users.noreply.github.com>
2024-10-31 09:43:51 +0000

    finalpush

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// // app.js
-// window.onload = () => {
-//     // Define your valid routes (the actual HTML files in your project)
-//     const validRoutes = ['index.html', 'faq.html', 'service.html', 'about.html', 'contact.html'];
+// app.js
+window.onload = () => {
+    // Define your valid routes (the actual HTML files in your project)
+    const validRoutes = ['index.html'];
 
-//     // Get the current path and extract the file name
-//     const currentPath = window.location.pathname.split('/').pop();
+    // Get the current path and extract the file name
+    const currentPath = window.location.pathname.split('/').pop();
 
-//     // If the current file name is not in the list of valid routes, redirect to the 404 page
-//     if (!validRoutes.includes(currentPath) && currentPath !== '') {
-//         window.location.href = '/404.html';  // Redirect to 404.html
-//     }
-// };
+    // If the current file name is not in the list of valid routes, redirect to the 404 page
+    if (!validRoutes.includes(currentPath) && currentPath !== '') {
+        window.location.href = 'index.html';  // Redirect to 404.html
+    }
+};
